b2112ed6f433dc326c08f2d1664e38aebb012668
shahsuresh <shahsuresh75@gmail.com>
2024-03-27 18:35:23 +0545

    router for cart item list  added

--- src/cart/cart.routes.js
+++ src/cart/cart.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { isBuyer } from "../middlewares/authentication.middleware.js";
 import validateReqBody from "../middlewares/validation.middleware.js";
-import { addItemToCartValidationSchema } from "./cart.validation.js";
+import {
+  addItemToCartValidationSchema,
+  updateCartQuantityValidationSchema,
+} from "./cart.validation.js";
 import mongoose from "mongoose";
 import Product from "../product/product.model.js";
 import Cart from "./cart.model.js";
+import validateMongoIdFromReqParams from "../middlewares/validateMongoID.middleware.js";
 
 const router = express.Router();
 
@@ -76,5 +80,139 @@ router.delete("/cart/clear", isBuyer, async (req, res) => {
   //remove cart items from logged in user
   await Cart.deleteMany({ buyerId: loggedInUserId });
   return res.status(200).send({ message: "Cart is cleared successfully" });
+});
+
+//?======remove single product from cart==========
+
+router.delete(
+  "/cart/item/remove/:id",
+  isBuyer,
+  validateMongoIdFromReqParams,
+  async (req, res) => {
+    // extract product id from req.params
+    const productId = req.params.id;
+
+    // find product by id
+    const product = await Product.findOne({ _id: productId });
+
+    // if not product, throw error
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exist" });
+    }
+
+    // remove product for this user from cart
+    await Cart.deleteOne({ buyerId: req.loggedInUserId, productId: productId });
+
+    // send res
+    return res
+      .status(200)
+      .send({ message: "Item is removed from Cart Successfully" });
+  }
+);
+
+//?======update item quantity in cart==========
+//use product id
+router.put(
+  "/cart/item/update/quantity/:id",
+  isBuyer,
+  validateReqBody(updateCartQuantityValidationSchema),
+  async (req, res) => {
+    //extract productId from req.params
+    const productId = req.params.id;
+
+    //extract buyerId from req.loggedInUserId
+    const buyerId = req.loggedInUserId;
+
+    //extract action from req.body
+    const actionData = req.body;
+
+    //find product using productId
+
+    const product = await Product.findOne({ _id: productId });
+
+    //if not product, throw error
+    if (!product) {
+      return res.status(404).send({ message: "Product does not exist" });
+    }
+
+    //find product's available Quanity
+    const productAvailableQuantity = product?.availableQuantity;
+
+    //find cart
+    const cartItem = await Cart.findOne({ buyerId, productId });
+
+    //if not cart item, throw error
+    if (!cartItem) {
+      return res.status(404).send({ message: "Cart item does not exist" });
+    }
+
+    //extract previous ordered quantity from cart item
+    let previousOrderedQuantity = cartItem.orderedQuantity;
+    let newOrderedQuantity;
+
+    if (actionData.action === "inc") {
+      newOrderedQuantity = previousOrderedQuantity + 1;
+    } else {
+      newOrderedQuantity = previousOrderedQuantity - 1;
+    }
+
+    if (newOrderedQuantity < 1) {
+      return res
+        .status(403)
+        .send({ message: "Ordered Quantity cannot be zero" });
+    }
+    if (newOrderedQuantity > productAvailableQuantity) {
+      return res
+        .status(403)
+        .send({ message: "Product Reached available Quantity" });
+    }
+    // update cart item with new ordered quantity
+    await Cart.updateOne(
+      { buyerId: buyerId, productId: productId },
+      {
+        $set: {
+          orderedQuantity: newOrderedQuantity,
+        },
+      }
+    );
+
+    return res
+      .status(200)
+      .send({ message: "Cart item quantity is updated successfully." });
+  }
+);
+
+//?===========list cart items=========
+
+router.get("/cart/item/list", isBuyer, async (req, res) => {
+  // extract buyerId from req.loggedInUserId
+  const buyerId = req.loggedInUserId;
+
+  const cartData = await Cart.aggregate([
+    {
+      $match: {
+        buyerId: buyerId,
+      },
+    },
+    {
+      $lookup: {
+        from: "products",
+        localField: "productId",
+        foreignField: "_id",
+        as: "productDetails",
+      },
+    },
+    {
+      $project: {
+        name: { $first: "$productDetails.name" },
+        brand: { $first: "$productDetails.brand" },
+        unitPrice: { $first: "$productDetails.price" },
+        image: { $first: "$productDetails.image" },
+        orderedQuantity: 1,
+      },
+    },
+  ]);
+
+  return res.status(200).send({ message: "success", cartData: cartData });
 });
 export default router;
--- src/cart/cart.validation.js
+++ src/cart/cart.validation.js
@@ -6,3 +6,8 @@ export const addItemToCartValidationSchema = Yup.object({
     .min(1, "Ordered quantity must be at least 1.")
     .required("Ordered quantity is required."),
 });
+
+//?======cart validation====
+export const updateCartQuantityValidationSchema = Yup.object({
+  action: Yup.string().oneOf(["inc", "dec"]).required("Action is required."),
+});
