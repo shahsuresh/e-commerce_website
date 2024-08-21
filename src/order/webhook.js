a637f9dee8be31f983818deed14918475b59318e
shahsuresh <shahsuresh75@gmail.com>
2024-08-22 00:40:26 +0545

    stripe payment webhook code and controller added

---
+++ src/order/webhook.js
@@ -0,0 +1,98 @@
+import Stripe from "stripe";
+import Cart from "../cart/cart.model.js";
+import orderModel from "./order.model.js";
+const stripe = Stripe(process.env.STRIPE_SECRET);
+const endpointSecret =
+  "whsec_edb3c6a11603d1b2603a74faf397ced1c46a6e22e1f9d3500773674f28f9ea62";
+
+//?======get lineitems==============
+async function getLIneItems(lineItems) {
+  let ProductItems = [];
+
+  if (lineItems?.data?.length) {
+    for (const item of lineItems.data) {
+      const product = await stripe.products.retrieve(item.price.product);
+      const productId = product.metadata.productID;
+      console.log("PRODUCT FROM WEBHOOK", product);
+      const productData = {
+        productId: productId,
+        name: product.name,
+        price: item.price.unit_amount / 100,
+        quantity: item.quantity,
+        image: product.images,
+      };
+      ProductItems.push(productData);
+      console.log("PRODUCT ITEMS", ProductItems);
+    }
+  }
+
+  return ProductItems;
+}
+
+//==================================
+
+const webhooks = async (request, response) => {
+  const sig = request.headers["stripe-signature"];
+  const payload = JSON.stringify(request.body);
+  const header = stripe.webhooks.generateTestHeaderString({
+    payload: payload,
+    secret: endpointSecret,
+  });
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(payload, header, endpointSecret);
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  //? Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const session = event.data.object;
+      // Then define and call a function to handle the event checkout.session.completed
+      const lineItems = await stripe.checkout.sessions.listLineItems(
+        session.id
+      );
+      console.log("LINEITEMS", lineItems);
+
+      const productDetails = await getLIneItems(lineItems);
+      const orderDetails = {
+        productDetails: productDetails,
+        email: session.customer_email,
+        userId: session.metadata.userId,
+        paymentDetails: {
+          paymentId: session.payment_intent,
+          payment_method_type: session.payment_method_types,
+          payment_status: session.payment_status,
+        },
+        shipping_options: session.shipping_options.map((s) => {
+          return {
+            ...s,
+            shipping_amount: s.shipping_amount / 100,
+          };
+        }),
+        totalAmount: session.amount_total / 100,
+      };
+      console.log("ORDER DETAILS WEBHOOK", orderDetails);
+      const order = new orderModel(orderDetails);
+      const saveOrder = await order.save();
+
+      if (saveOrder?._id) {
+        //! ==on payment success, delete all products from that user cart====
+        const deleteCartItem = await Cart.deleteMany({
+          buyerId: session.metadata.userId,
+        });
+        //TODO: send email to user , containing user order details
+      }
+
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  response.status(200).send();
+};
+
+export default webhooks;
--- src/payment-mode/stripe.payments.js
+++ src/payment-mode/stripe.payments.js
@@ -1,11 +1,20 @@
 import Stripe from "stripe";
-const stripe = Stripe(process.env.STRIPE_SECRET);
+const stripe = new Stripe(process.env.STRIPE_SECRET);
 // console.log(stripe);
 import { Router } from "express";
+import User from "../user/user.model.js";
+import { isBuyer } from "../middlewares/authentication.middleware.js";
 const router = Router();
 
-router.post("/stripe-pay", async (req, res) => {
+// router.post("/stripe-pay",
+const stripePaymentController = async (req, res) => {
   const { products } = req.body;
+  console.log(products);
+
+  const buyerId = req.loggedInUserId;
+  console.log("BUYER ID IS", buyerId);
+  const user = await User.findOne({ _id: buyerId });
+  console.log(user);
 
   const lineItems = products.map((product) => ({
     price_data: {
@@ -14,6 +23,7 @@ router.post("/stripe-pay", async (req, res) => {
         name: product.name,
         // ...(product.image ? { images: [image] } : {}),
         // images: [product.image],
+        metadata: { productID: product._id },
       },
       unit_amount: product.unitPrice * 100,
     },
@@ -23,7 +33,13 @@ router.post("/stripe-pay", async (req, res) => {
   //   console.log("Line ITEMS::", lineItems);
   //   console.log(lineItems[0].price_data.product_data.name);
   const session = await stripe.checkout.sessions.create({
+    submit_type: "pay",
     payment_method_types: ["card"],
+    billing_address_collection: "auto",
+    //customer_email: "user?.email",
+    metadata: {
+      userId: buyerId,
+    },
     line_items: lineItems,
     mode: "payment",
     success_url: "http://localhost:5173/payment-success",
@@ -31,6 +47,7 @@ router.post("/stripe-pay", async (req, res) => {
   });
   //   console.log("SESSION BACKEND", session);
   res.json({ id: session.id });
-});
+};
 
-export default router;
+// export default router;
+export default stripePaymentController;
